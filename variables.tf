71760f7304549c944428df1c017688841659c7db
Jack Cusick <jmorgancusick@gmail.com>
2024-03-07 16:49:54 -0700

    Sample s3 bucket

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "The region to use when configuring the AWS provider"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "owner" {
+  description = "This project's owner, which will be added as a tag to resources"
+  type        = string
+  default     = "Jack Cusick"
+}
